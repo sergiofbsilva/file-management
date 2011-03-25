1dfcdbe0ab26dd1037cb3480a2fff7700aa85c79
Luis Cruz <luis.cruz@ist.utl.pt>
2011-03-25 14:07:48 +0000

    Minor Change: Interface to adding code to actions.

---
+++ file-management/src/module/fileManagement/presentationTier/component/ActionWithImplementation.java
@@ -0,0 +1,18 @@
+package module.fileManagement.presentationTier.component;
+
+import com.vaadin.event.Action;
+import com.vaadin.terminal.Resource;
+
+public abstract class ActionWithImplementation extends Action {
+
+    public ActionWithImplementation(String caption) {
+	super(caption);
+    }
+
+    public ActionWithImplementation(final String caption, final Resource resource) {
+	super(caption, resource);
+    }
+
+    public abstract void perform(final Object target);
+
+}
