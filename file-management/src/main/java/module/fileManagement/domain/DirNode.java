333787b7971940601bd7e5f4e52233ae657fdf41
sergiofbsilva <sergiofbsilva@gmail.com>
2013-06-11 15:55:48 +0100

    Move relation listener to proper place due to class loading bug

--- file-management/src/main/java/module/fileManagement/domain/DirNode.java
+++ file-management/src/main/java/module/fileManagement/domain/DirNode.java
@@ -35,6 +35,9 @@ import pt.ist.bennu.core.domain.groups.Role;
 import pt.ist.bennu.core.domain.groups.SingleUserGroup;
 import pt.ist.bennu.core.domain.groups.UnionGroup;
 import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.dml.runtime.DirectRelation;
+import pt.ist.fenixframework.dml.runtime.Relation;
+import pt.ist.fenixframework.dml.runtime.RelationListener;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
@@ -55,6 +58,35 @@ public class DirNode extends DirNode_Base {
     static {
         INVALID_USER_DIR_NAMES.add(SHARED_DIR_NAME);
         INVALID_USER_DIR_NAMES.add(TRASH_DIR_NAME);
+
+        DirectRelation<DirNode, AbstractFileNode> child = getRelationDirNodeAbstractFileNode();
+        child.addListener(new RelationListener<DirNode, AbstractFileNode>() {
+
+            @Override
+            public void afterAdd(Relation<DirNode, AbstractFileNode> arg0, DirNode arg2, AbstractFileNode arg1) {
+                if (arg1 != null && arg2 != null && !arg1.isShared()) {
+                    arg2.addUsedSpace(arg1.getFilesize());
+                }
+            }
+
+            @Override
+            public void afterRemove(Relation<DirNode, AbstractFileNode> arg0, DirNode arg2, AbstractFileNode arg1) {
+                if (arg1 != null && arg2 != null && !arg1.isShared()) {
+                    arg2.removeUsedSpace(arg1.getFilesize());
+                }
+            }
+
+            @Override
+            public void beforeAdd(Relation<DirNode, AbstractFileNode> arg0, DirNode arg2, AbstractFileNode arg1) {
+
+            }
+
+            @Override
+            public void beforeRemove(Relation<DirNode, AbstractFileNode> arg0, DirNode arg2, AbstractFileNode arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 
     // constructors
