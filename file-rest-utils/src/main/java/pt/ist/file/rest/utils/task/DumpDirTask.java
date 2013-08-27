5785cfbd617222039a855e776ccf06988c04f5d6
sergiofbsilva <sergiofbsilva@gmail.com>
2013-08-27 19:30:27 +0100

    refactor file-rest-utils to bennu2

--- file-rest-utils/src/main/java/pt/ist/file/rest/utils/task/DumpDirTask.java
+++ file-rest-utils/src/main/java/pt/ist/file/rest/utils/task/DumpDirTask.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 import org.apache.commons.lang.StringUtils;
 
 import pt.ist.bennu.core.domain.User;
-import pt.ist.bennu.core.domain.scheduler.WriteCustomTask;
+import pt.ist.bennu.scheduler.custom.CustomTask;
 import pt.ist.file.rest.utils.client.FMSRestClient;
 import pt.ist.file.rest.utils.client.Folder;
 import pt.ist.file.rest.utils.client.IDocument;
@@ -16,18 +16,18 @@ import pt.ist.file.rest.utils.domain.FileRestUtilsSystem;
 import pt.ist.file.rest.utils.domain.RemoteDocument;
 import pt.ist.file.rest.utils.domain.RemoteFolder;
 
-public class DumpDirTask extends WriteCustomTask {
+public class DumpDirTask extends CustomTask {
     private void copyFiles(final File localDir, final Folder remoteDir, final int depth) {
         final Map<String, String> map = new HashMap<String, String>();
         for (final File file : localDir.listFiles()) {
             if (file.isDirectory()) {
                 final String dirName = file.getName();
                 if (!dirName.contains(".svn")) {
-                    out.printf("%s[D] %s\n", StringUtils.repeat("-", depth), dirName);
+                    taskLog("%s[D] %s\n", StringUtils.repeat("-", depth), dirName);
                     copyFiles(file, remoteDir.createFolder(dirName), depth + 1);
                 }
             } else {
-                out.printf("%s[F] %s\n", StringUtils.repeat("-", depth), file.getName());
+                taskLog("%s[F] %s\n", StringUtils.repeat("-", depth), file.getName());
                 final IDocument doc = remoteDir.createDocument(file);
                 map.clear();
                 map.put("Caminho local", file.getAbsolutePath());
@@ -51,12 +51,12 @@ public class DumpDirTask extends WriteCustomTask {
         final File file2upload = new File("/home/sfbs/Desktop/dmatos-full.html");
         final Folder testRest = myRootDirectory.createFolder("testREST");
         final IDocument doc = testRest.createDocument(file2upload);
-        System.out.println(doc.info());
+        taskLog(doc.info());
         final Map<String, String> map = new HashMap<String, String>();
         map.put("x", "1");
         map.put("y", "2");
         map.put("z", "3");
-        System.out.println(doc.metadata(map));
+        taskLog(doc.metadata(map));
     }
 
     private void testPersist(final Folder myRootDirectory) {
@@ -64,11 +64,11 @@ public class DumpDirTask extends WriteCustomTask {
         final FileRestUtilsSystem system = FileRestUtilsSystem.getInstance();
         if (system.getFolderSet().isEmpty()) {
             system.addFolder(myRootDirectory.createFolder("testPersist").persist());
-            out.println("create testPersist");
+            taskLog("create testPersist");
         }
         if (system.getDocumentSet().isEmpty()) {
             for (final RemoteDocument doc : system.getDocument()) {
-                out.println(doc.info());
+                taskLog(doc.info());
             }
         }
         final RemoteFolder remoteFolder = system.getFolder().iterator().next();
@@ -76,11 +76,11 @@ public class DumpDirTask extends WriteCustomTask {
         final Folder folder = remoteFolder.createFolder(random);
         final RemoteDocument doc = folder.createDocument(file2upload).persist();
         doc.setSystem(system);
-        out.println("create dir " + random);
+        taskLog("create dir " + random);
     }
 
     @Override
-    protected void doService() {
+    public void runTask() {
         final FMSRestClient fms = new FMSRestClient("dot", User.findByUsername("ist152416").getUsername());
         final Folder rootDir = fms.getRootDirectory();
         // uploadSingleFile(rootDir);
